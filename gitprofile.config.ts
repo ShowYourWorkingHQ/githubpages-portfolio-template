62b6690712aa889cc05eea2d16d04f0a88c4c84f
Luna Underwood <113502158+lunahhhhhhhhhhhhhhhh@users.noreply.github.com>
2024-10-13 14:20:38 +1300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -1,51 +1,217 @@
 // gitprofile.config.ts
+export type SocialLinks = {
+  /** LinkedIn username. Example: "ariful-alam" */
+  linkedin?: string;
+  /** Twitter username. Example: "arif_szn" */
+  twitter?: string;
+  /** Mastodon username. Example: "arifszn@mastodon.social" */
+  mastodon?: string;
+  /** Personal website URL. Example: "https://www.arifszn.com" */
+  website?: string;
+  /** Contact email address. Example: "arifulalamszn@gmail.com" */
+  email?: string;
+};
 
-const CONFIG = {
+export type Project = {
+  /** Project title. Example: "Project Name" */
+  title: string;
+  /** Project description. Example: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." */
+  description: string;
+  /** URL for the project image. Example: "https://example.com/image.jpg" */
+  imageUrl: string;
+  /** URL to the project. Example: "https://example.com" */
+  link: string;
+};
+
+export type Experience = {
+  /** Company name. Example: "Company Name" */
+  company: string;
+  /** Position held. Example: "Position" */
+  position: string;
+  /** Start date. Example: "September 2021" */
+  from: string;
+  /** End date. Use "Present" for current positions. Example: "Present" */
+  to: string;
+  /** URL to company website. Example: "https://example.com" */
+  companyLink: string;
+};
+
+export type Education = {
+  /** Name of the institution. Example: "Institution Name" */
+  institution: string;
+  /** Degree obtained. Example: "Degree" */
+  degree: string;
+  /** Start year. Example: "2015" */
+  from: string;
+  /** End year. Example: "2019" */
+  to: string;
+};
+
+export type Publication = {
+  /** Title of the publication. Example: "Publication Title" */
+  title: string;
+  /** Name of the journal (optional). Example: "Journal Name" */
+  journalName?: string;
+  /** Name of the conference (optional). Example: "Conference Name" */
+  conferenceName?: string;
+  /** Authors of the publication. Example: "John Doe, Jane Smith" */
+  authors: string;
+  /** URL to the publication. Example: "https://example.com" */
+  link: string;
+  /** Brief description of the publication. Example: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." */
+  description: string;
+};
+
+export type Config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    /** Your GitHub org/user name. (This is the only required config) Example: "arifszn" */
+    username: string;
+  };
+  /** Base URL for deployment. Example: "/gitprofile/" */
+  base: string;
+  projects: {
+    github: {
+      /** Display GitHub projects? Example: true */
+      display: boolean;
+      /** Header for GitHub projects section. Example: "Github Projects" */
+      header: string;
+      /** Mode can be: automatic or manual. Example: "automatic" */
+      mode: 'automatic' | 'manual';
+      automatic: {
+        /** Sort projects by stars or updated. Example: "stars" */
+        sortBy: 'stars' | 'updated';
+        /** How many projects to display. Example: 8 */
+        limit: number;
+        exclude: {
+          /** Forked projects will not be displayed if set to true. Example: false */
+          forks: boolean;
+          /** These projects will not be displayed. Example: [] */
+          projects: string[];
+        };
+      };
+      manual: {
+        /** List of repository names to display. Example: ["arifszn/gitprofile", "arifszn/pandora"] */
+        projects: string[];
+      };
+    };
+    external: {
+      /** Header for external projects section. Example: "My Projects" */
+      header: string;
+      /** List of external projects. */
+      projects: Project[];
+    };
+  };
+  seo: {
+    /** Title for SEO. Example: "Portfolio of Ariful Alam" */
+    title: string;
+    /** Description for SEO. Example: "" */
+    description: string;
+    /** Image URL for SEO. Example: "" */
+    imageURL: string;
+  };
+  social: SocialLinks;
+  resume: {
+    /** URL for resume file. Empty will hide the Download Resume button. Example: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" */
+    fileUrl: string;
+  };
+  /** List of skills. Example: ["PHP", "Laravel", "JavaScript", "React.js", ...] */
+  skills: string[];
+  /** List of work experiences. */
+  experiences: Experience[];
+  certifications: {
+    /** Name of the certification. Example: "Lorem ipsum" */
+    name: string;
+    /** Issuing body. Example: "Lorem ipsum dolor sit amet" */
+    body: string;
+    /** Year of certification. Example: "March 2022" */
+    year: string;
+    /** URL to the certification. Example: "https://example.com" */
+    link: string;
+  }[];
+  /** List of educational qualifications. */
+  educations: Education[];
+  /** List of publications. */
+  publications: Publication[];
+  blog: {
+    /** Source for blog articles (medium | dev). Example: "dev" */
+    source: 'medium' | 'dev';
+    /** Username for blog source. To hide blog section, keep it empty. Example: "arifszn" */
+    username: string;
+    /** How many articles to display. Max is 10. Example: 2 */
+    limit: number;
+  };
+  googleAnalytics: {
+    /** GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX. Example: "" */
+    id: string;
+  };
+  hotjar: {
+    /** Hotjar ID. Example: "" */
+    id: string;
+    /** Hotjar snippet version. Example: 6 */
+    snippetVersion: number;
+  };
+  themeConfig: {
+    /** Default theme for the application. Example: "lofi" */
+    defaultTheme: string;
+    /** Hides the theme switch in the navbar. Example: false */
+    disableSwitch: boolean;
+    /** Uses user system preferences for theme. Example: false */
+    respectPrefersColorScheme: boolean;
+    /** Displays a ring around the profile picture. Example: true */
+    displayAvatarRing: boolean;
+    /** Available themes. Example: ["light", "dark", "cupcake", ...] */
+    themes: string[];
+    customTheme: {
+      primary: string;
+      secondary: string;
+      accent: string;
+      neutral: string;
+      'base-100': string;
+      '--rounded-box': string;
+      '--rounded-btn': string;
+    };
+  };
+  /** Optional footer text or HTML. Example: "Made with <a class="text-primary" href="https://github.com/arifszn/gitprofile" target="_blank" rel="noreferrer">GitProfile</a> and ❤️" */
+  footer: string;
+  /** Enable Progressive Web App features. Example: true */
+  enablePWA: boolean;
+};
+
+const CONFIG: Config = {
+  github: {
+    username: 'arifszn',
   },
-  /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
-   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
-   */
   base: '/gitprofile/',
   projects: {
     github: {
-      display: true, // Display GitHub projects?
+      display: true,
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'automatic',
       automatic: {
-        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        sortBy: 'stars',
+        limit: 8,
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          forks: false,
+          projects: [],
         },
       },
       manual: {
-        // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['arifszn/gitprofile', 'arifszn/pandora'],
       },
     },
     external: {
       header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
       projects: [
         {
           title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl: 'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
         },
         {
           title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl: 'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
         },
       ],
@@ -60,27 +226,11 @@ const CONFIG = {
     linkedin: 'ariful-alam',
     twitter: 'arif_szn',
     mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
-    instagram: '',
-    reddit: '',
-    threads: '',
-    youtube: '', // example: 'pewdiepie'
-    udemy: '',
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
     website: 'https://www.arifszn.com',
-    phone: '',
     email: 'arifulalamszn@gmail.com',
   },
   resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
   },
   skills: [
     'PHP',
@@ -139,52 +289,36 @@ const CONFIG = {
   publications: [
     {
       title: 'Publication Title',
-      conferenceName: '',
       journalName: 'Journal Name',
       authors: 'John Doe, Jane Smith',
       link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
       title: 'Publication Title',
       conferenceName: 'Conference Name',
-      journalName: '',
       authors: 'John Doe, Jane Smith',
       link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
   ],
-  // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    source: 'dev',
+    username: 'arifszn',
+    limit: 2,
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: '',
   },
-  // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
     id: '',
     snippetVersion: 6,
   },
   themeConfig: {
     defaultTheme: 'lofi',
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
     disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
-
-    // Display the ring in Profile picture
     displayAvatarRing: true,
-
-    // Available themes. To remove any theme, exclude from here.
     themes: [
       'light',
       'dark',
@@ -220,8 +354,6 @@ const CONFIG = {
       'sunset',
       'procyon',
     ],
-
-    // Custom theme, applied to `procyon` theme
     customTheme: {
       primary: '#fc055b',
       secondary: '#219aaf',
@@ -232,14 +364,7 @@ const CONFIG = {
       '--rounded-btn': '3rem',
     },
   },
-
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
-
+  footer: 'Made with <a class="text-primary" href="https://github.com/arifszn/gitprofile" target="_blank" rel="noreferrer">GitProfile</a> and ❤️',
   enablePWA: true,
 };
 
